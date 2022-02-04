06d69fe29c1f63a938effad8093f4656f0073d7c
orezra37 <74672297+orezra37@users.noreply.github.com>
2022-02-04 21:28:21 +0200

    Main script to run

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import torch
 from our_models.OG_former import OG1
-from train_loop import train_naive as train
+from train_naive import train_naive as train
 
 num_heads = 4
 lr = 1e-2
 epochs = 10
 batch_size = 4
 
-model = OG1(num_heads=num_heads)
-train(model=model,
+
+naive_model_1 = OG1(num_heads=num_heads)
+train(model=naive_model_1,
       batch_size=batch_size,
       num_heads=num_heads,
       lr=lr,
       epochs=epochs)
+
+path = "Naive_model_state_dict"
+print(path)
+torch.save(naive_model_1.state_dict(), path)
+
--- train_loop.py
+++ train_loop.py
@@ -49,29 +49,6 @@ def train(args):
         #     loss = criterion(pred, y)
 
 
-def train_naive(model=None, batch_size=4, num_heads=4, lr=1e-2, epochs=10):
-    """Train loop for OG-former based on a naive architecture."""
-    train_dataset = OGDataset("data")
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-    criterion = CrossEntropyLoss()
-    if model is None: model = OG1(num_heads=num_heads)
-    model.cuda()
-    optimizer = Adam(model.parameters(), lr=lr)
-
-    for e in range(epochs):
-        print(e)
-        model.train()
-
-        for i_batch, batch in enumerate(train_loader):
-            x, y = batch
-            pred = model(x)
-            loss = criterion(pred, y[0])
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            print(loss.item())
-
 if __name__ == '__main__':
     import pydevd_pycharm
     pydevd_pycharm.settrace('10.96.2.219', port=123, stdoutToServer=True, stderrToServer=True)

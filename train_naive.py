829bf81c22f80d023243d09244beffdb8df8244e
orezra37 <74672297+orezra37@users.noreply.github.com>
2022-02-14 00:05:15 +0200

    changes

--- train_naive.py
+++ train_naive.py
@@ -5,15 +5,10 @@ from our_models.OG_former import OG1
 from torch.optim import Adam
 
 
-def train_naive(model=None, batch_size=4, num_heads=4, lr=1e-2, epochs=10):
+def train_naive(model, criterion=CrossEntropyLoss, lr=1e-2, batch_size=4, epochs=10):
     """Train loop for OG-former based on a naive architecture."""
     train_dataset = OGDataset("data")
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-    criterion = CrossEntropyLoss()
-    if model is None:
-        model = OG1(num_heads=num_heads)
-    # model.cuda()
     optimizer = Adam(model.parameters(), lr=lr)
 
     for e in range(epochs):
@@ -21,7 +16,7 @@ def train_naive(model=None, batch_size=4, num_heads=4, lr=1e-2, epochs=10):
         model.train()
 
         for i_batch, batch in enumerate(train_loader):
-            x, y = batch
+            x, y, _ = batch
             pred = model(x)
             loss = criterion(pred, y[0])
             optimizer.zero_grad()

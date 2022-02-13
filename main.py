2090a14a31f823cc33641cd3291755275717f01f
orezra37 <74672297+orezra37@users.noreply.github.com>
2022-02-14 00:03:29 +0200

    prepare to train

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ batch_size = 4
 
 my_dataset = OGDataset("data")
 print('length of dataset:', len(my_dataset))
-x, y = my_dataset[0]
-s, z = x
-print('S sample:', s.size(),
-      '\nz sample:', z.size(),
-      '\naatype sample:', y.size())
 
-# naive_model_1 = OG1(num_heads=num_heads)
-# train(model=naive_model_1,
-#       batch_size=batch_size,
-#       num_heads=num_heads,
-#       lr=lr,
-#       epochs=epochs)
+# x, y = my_dataset[0]
+# s, z = x
+# print('S sample:', s.size(),
+#       '\nz sample:', z.size(),
+#       '\naatype sample:', y.size())
+
+naive_model_1 = OG1(num_heads=num_heads)
+train(model=naive_model_1,
+      batch_size=batch_size,
+      num_heads=num_heads,
+      lr=lr,
+      epochs=epochs)
 
 # path = "Naive_model_state_dict"
 # print(path)

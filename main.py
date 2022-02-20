d542820029262e0989ec431f9390d323463f1761
orezra37 <74672297+orezra37@users.noreply.github.com>
2022-02-21 01:45:00 +0200

    debugging until it runs on moba

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from OGDataset import OGDataset
 
 num_heads = 4
 lr = 1e-2
-epochs = 1e3
+epochs = 20
 batch_size = 1
+test_every = 1
+
 
 my_dataset = OGDataset("data")
 print('length of dataset:', len(my_dataset))
 
 naive_model_1 = OG(num_heads=num_heads)
+
 train(model=naive_model_1,
       batch_size=batch_size,
       lr=lr,
       epochs=epochs,
-      train_dataset=my_dataset)
+      train_dataset=my_dataset,
+      test_every=test_every)
 
 # path = "Naive_model_state_dict"
 # print(path)
--- train_naive.py
+++ train_naive.py
@@ -6,29 +6,30 @@ from our_models.OG_former import OG
 from torch.optim import Adam
 
 
-def train_naive(model, criterion=CrossEntropyLoss(), lr=1e-2, batch_size=4, epochs=10, train_dataset=OGDataset("data"),
+def train_naive(model, lr=1e-2, batch_size=4, epochs=10, train_dataset=OGDataset("data"),
                 test_every=10):
     """Train loop for OG-former based on a naive architecture."""
+    criterion = CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=lr)
     model = model.to(train_dataset.device)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    optimizer = Adam(model.parameters(), lr=lr)
-    for e in range(1, 1 + epochs):
+    for e in range(1, 1 + int(epochs)):
         if e % test_every == 0:
             print('epoch:', e)
-        model.train()
         optimizer.zero_grad()
+        model.train()
         running_loss = 0
         for i_batch, batch in enumerate(train_loader):
             x, y, _ = batch
             s, z = x
             probs = model(s, z)
-            _, prediction = torch.max(probs.data, 2)
-            loss = criterion(probs[0], y[0])
-            correct = (prediction == y).sum()
-            accuracy = torch.round(100 * correct / s.size(1)).item()
+            loss = criterion(probs[i_batch], y[i_batch])
+            prediction = torch.argmax(probs.data, dim=2)
             loss.backward()
             optimizer.step()
-        running_loss += loss.item()
+            running_loss += loss.item()
+            correct = (prediction == y).sum()
+            accuracy = torch.round(100 * correct / s.size(1)).item()
         if e % test_every == 0:
             print('loss', running_loss)
             print('accuracy', accuracy, '\n')

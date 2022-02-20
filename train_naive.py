287e7cddcfa6d9ba6d47df8cf4fac21d94b48144
orezra37 <74672297+orezra37@users.noreply.github.com>
2022-02-21 00:47:16 +0200

    debugging until it runs on moba

--- train_naive.py
+++ train_naive.py
@@ -6,24 +6,29 @@ from our_models.OG_former import OG
 from torch.optim import Adam
 
 
-def train_naive(model, criterion=CrossEntropyLoss(), lr=1e-2, batch_size=4, epochs=10, train_dataset=OGDataset("data")):
+def train_naive(model, criterion=CrossEntropyLoss(), lr=1e-2, batch_size=4, epochs=10, train_dataset=OGDataset("data"),
+                test_every=10):
     """Train loop for OG-former based on a naive architecture."""
     model = model.to(train_dataset.device)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     optimizer = Adam(model.parameters(), lr=lr)
-    for e in range(epochs):
-        print('epoch:', e)
+    for e in range(1, 1 + epochs):
+        if e % test_every == 0:
+            print('epoch:', e)
         model.train()
         optimizer.zero_grad()
+        running_loss = 0
         for i_batch, batch in enumerate(train_loader):
             x, y, _ = batch
             s, z = x
             probs = model(s, z)
             _, prediction = torch.max(probs.data, 2)
             loss = criterion(probs[0], y[0])
-            correct = (prediction == y).sum().item()
-            accuracy = 100 * correct / s.size(1)
+            correct = (prediction == y).sum()
+            accuracy = torch.round(100 * correct / s.size(1)).item()
             loss.backward()
             optimizer.step()
-            print('loss', loss.item())
+        running_loss += loss.item()
+        if e % test_every == 0:
+            print('loss', running_loss)
             print('accuracy', accuracy, '\n')
